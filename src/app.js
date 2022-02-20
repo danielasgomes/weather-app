9c9074becb161021222baf756c5c2abca6587b12
Daniela Gomes <danisgomes98@gmail.com>
2022-02-20 17:32:52 +0000

    Removed console.log

--- src/app.js
+++ src/app.js
@@ -23,13 +23,13 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
   let temperature = document.querySelector("#current-temperature");
   let city = document.querySelector("#city");
   let description = document.querySelector("#infoData");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
   let dateElement = document.querySelector("#current-day");
+
   temperature.innerHTML = Math.round(response.data.main.temp);
   city.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
@@ -39,6 +39,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "d301f37ab69ea027ae4b1946ff4c3c0b";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
