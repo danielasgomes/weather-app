3c41a04749d28d868da8a326059074abfd126b58
Daniela Gomes <danisgomes98@gmail.com>
2022-02-20 17:09:25 +0000

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-alert("hello");
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperature = document.querySelector("#current-temperature");
+  let city = document.querySelector("#city");
+  let description = document.querySelector("#infoData");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  city.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "d301f37ab69ea027ae4b1946ff4c3c0b";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
